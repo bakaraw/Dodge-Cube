37f0278d218d3057c4ce46382045c15b2be4d2fd
bakaraw <142462547+bakaraw@users.noreply.github.com>
2024-07-08 22:17:18 +0800

    Add player class to make it cleaner

---
+++ src/entities/Player.cpp
@@ -0,0 +1,36 @@
+//
+// Created by bacal on 7/8/2024.
+//
+
+#include "Player.h"
+
+Player::Player( glm::vec3 initialPosition): Position(initialPosition){
+
+}
+
+void Player::processInput(Direction direction, float deltaTime) {
+    float velocity = playerSpeed * deltaTime;
+
+    if (direction == LEFT)
+        Position.x += velocity;
+
+    if (direction == RIGHT)
+        Position.x -= velocity;
+
+    if (Position.x > 3.0f)
+        Position.x = 3.0f;
+
+    if (Position.x <= -3.0f)
+        Position.x = -3.0f;
+}
+
+
+void Player::update(Shader &shader) {
+    shader.use();
+    glm::mat4 Model = glm::mat4(1.0f);
+    Model = glm::translate(Model, Position);
+    shader.setMat4("model", Model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+
---
+++ src/entities/Player.h
@@ -0,0 +1,35 @@
+//
+// Created by bacal on 7/8/2024.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "../graphics/Shader.h"
+
+enum Direction {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+class Player {
+public:
+    glm::vec3 Position;
+    int playerDirection;
+    float playerSpeed = 2.5f;
+    float deltaTime = 0.0f;
+
+    Player(glm::vec3 initialPosition);
+    void processInput(Direction direction, float deltaTime);
+    void update(Shader &shader);
+
+};
+
+
+
+#endif //PLAYER_H
